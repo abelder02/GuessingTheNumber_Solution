47c2d274fd4592906d7c2029fedfa5def7d5b2a1
abelder02 <37052864+abelder02@users.noreply.github.com>
2018-05-23 21:42:39 -0400

    Add project files.

---
+++ Ch8_Program2/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Ch8_Program2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(217, 292);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(332, 107);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Guess";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(100, 57);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(517, 35);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Guess a number between 0 and 100:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(301, 116);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(193, 45);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(81, 224);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(592, 32);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "YOU GUESSED THE NUMBER CORRECLTY";
+            this.label2.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(369, 239);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            this.label3.Visible = false;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.ClientSize = new System.Drawing.Size(777, 439);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Ch8_Program2/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Ch8_Program2
+{
+    public partial class Form1 : Form
+    {
+        public Random Num = new Random();
+        public int randNum;
+        public int UserInput;
+        public int R = 25;
+        public int G = 144;
+        public int B = 2;
+        public int length;
+        
+        
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+            
+            BackColor = Color.FromArgb(R, G, B);
+
+            //BackColor = ChangeColor();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //label2.Visible = false;
+            UserInput = Convert.ToInt32(textBox1.Text);
+            randNum = 7;// Num.Next(1, 101);
+            if (UserInput == randNum)
+            {
+                R = 0; G = 255; B = 0;
+                label2.Text = "CORRECT! YOU GOT IT!!!";
+                label2.Visible = true;
+                for (int i = 0; i < 3; i++)
+                {
+                    R += 50; B += 50; G -= 50;
+                    BackColor = Color.FromArgb(R, G, B);
+                    
+                    Thread.Sleep(105);
+                }
+                
+                Thread.Sleep(100);
+                
+                                               
+            }
+            if(UserInput > randNum)
+            {
+                R = 255;
+                G = 0;
+                B = 0;
+                length = (UserInput - randNum);
+                label2.Text = "Lower...";
+                label2.Visible = true;
+                for (int i = 0; i < length; i++)
+                {
+                    R = (int)(R - 1.5);
+                }
+                
+                
+
+            }
+            if(UserInput < randNum)
+            {
+                R = 0;
+                G = 0;
+                B = 255;
+                length = (UserInput - randNum);
+                label2.Text = "Higher...";
+                label2.Visible = true;
+                for (int i = 0; i < length; i++)
+                {
+                    B = (int)(B-1.5);
+                }
+                BackColor = Color.FromArgb(R, G, B);
+                
+
+            }
+            bool goOn = false, flag = true;
+            int timer = 0;
+            while(flag)
+            {
+                Thread.Sleep(10);
+                timer++;
+                if (timer > 100)
+                {
+                    goOn = true; flag = false;
+                }
+            }           
+            label2_Click(goOn, e);
+        }
+
+        private void button1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            //label2.Visible = true;
+            Thread.Sleep(5000);
+            //label2.ResetText();
+            bool goOn = false, flag = true;
+            int timer = 0;
+            while (flag)
+            {
+                Thread.Sleep(1);
+                timer++;
+                if (timer > 10)
+                {
+                    goOn = true; flag = false;
+                }
+            }
+            label3_Click(goOn, e);
+
+
+        }
+
+        private void label2_Paint(object sender, PaintEventArgs e)
+        {
+            label2.BackColor = Color.DarkSlateBlue;
+        }
+
+        private void Form1_Paint(object senderer, PaintEventArgs e)
+        {
+
+            BackColor = Color.FromArgb(R, G, B);
+        }
+
+        private void button1_DragOver(object sender, DragEventArgs e)
+        {
+            button1.BackColor = Color.Azure;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            label2.Visible = false;
+            label3.Visible = true;
+            label3.Text = ("It was: " + randNum);
+            bool goOn = false, flag = true;
+            int timer = 0;
+            while (flag)
+            {
+                Thread.Sleep(10);
+                timer++;
+                if (timer > 10)
+                {
+                }
+            }
+            
+        }
+    }
+}
